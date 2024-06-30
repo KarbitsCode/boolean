02e13abaff6bec4efe1b812b34b50e78a171f42a
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2024-06-30 11:58:54 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -29,4 +29,15 @@ setup(name='boolean',
       zip_safe=False,
       test_suite="test",
       install_requires=['regex'],
+      classifiers=[
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+      ],
       )
